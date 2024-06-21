78ce1c1bc7fc9feb4fbc169e1502a240d3dc4f04
Waimea-RDNicholls <rdnicholls@waimea.school.nz>
2024-06-21 12:47:42 +1200

    register+login action+form+page

--- site/views/pages/home.php
+++ site/views/pages/home.php
@@ -1,4 +1,26 @@
-<h1>Hello from the Simple PHP Router!</h1>
+<?php global $isLoggedIn; ?>
+
+
+<?php if ($isLoggedIn): ?>
+    <h1>Welcome, [user]</h1>
+
+    <?php else: ?>
+
+    <h1>Welcome to GameLinker!</h1>
+<p>It seems you aren't signed in.</p>
+<div>
+    <p
+    id="register"
+        hx-get="/register"
+        hx-trigger="load">Sign Up!</p>
+    <p
+    id="login"
+        hx-get="/login"
+        hx-trigger="load">Log In!</p>
+</div>
+<?php endif; ?>
+
+
 
 
 
